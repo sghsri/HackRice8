8cafbc715c3414791f5986cc36fd0fd5ab26b05c
10001shh <sghsri@gmail.com>
2018-09-15 19:34:40 -0500

    room spotify id

--- js/create.js
+++ js/create.js
@@ -7,31 +7,28 @@ $(function () {
     $("#create").click(e => {
         var party = {
             question: $("#question").val(),
+            spotify_id: $("#spotify_id").val(),
             answers: []
         }
-        if (party.question.length > 0) {
-            if ($('input[name="answer"]').length) {
-                $('input[name="answer"]').each(function () {
-                    party.answers.push({
-                        answertext: $(this).val(),
-                        votes: 0
-                    })
-                });
-                var xhttp = new XMLHttpRequest();
-                xhttp.onreadystatechange = function () {
-                    if (this.readyState == 4 && this.status == 200) {
-                        console.log(this.responseText);
-                        window.location.href = `/host/${this.responseText}`;
-                    }
-                };
-                xhttp.open("POST", `/create`, true);
-                xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-                xhttp.send(JSON.stringify(party));
-            } else {
-                alert("Must have at least 1 answer choice!");
-            }
+        if (party.question.length > 0 && party.spotify_id.length > 0) {
+            $('input[name="answer"]').each(function () {
+                party.answers.push({
+                    answertext: $(this).val(),
+                    votes: 0
+                })
+            });
+            var xhttp = new XMLHttpRequest();
+            xhttp.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(this.responseText);
+                    window.location.href = `/host/${this.responseText}`;
+                }
+            };
+            xhttp.open("POST", `/create`, true);
+            xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+            xhttp.send(JSON.stringify(party));
         } else {
-            alert("Must have a Question!");
+            alert("Must have a Party Name and Spotify ID!");
         }
     });
 });
